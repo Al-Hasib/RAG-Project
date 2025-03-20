5481fb9616091288faf7e36b24b0762922951e4e
Md Abdullah  Al Hasib <mdabdullahalhasib1111@gmail.com>
2025-03-20 23:47:45 +0600

    update

--- app_test.py
+++ app_test.py
@@ -17,14 +17,15 @@
 
 from src.utils import retreive_db
 from src.chat import rag_chain
+from src.create_vector import create_knowledgebase
 # load_dotenv()
-# data_path = r"F:\Projects\RAG-with-Qdrant\data\150-WBE_500-TDS.pdf"
+data_path = "/root/RAG-Project/data/150-WBE_500-TDS.pdf"
 
-# # create_knowledgebase(data_path)
+create_knowledgebase(data_path)
 # db = retreive_db()
 # docs = db.similarity_search("What is the purpose of the document?")
 # print(f"{docs[0].page_content}\n\n")
 
-result = rag_chain.invoke("What is the purpose of the document?")
+# result = rag_chain.invoke("What is the purpose of the document?")
 
-print(result)
+# print(result)
